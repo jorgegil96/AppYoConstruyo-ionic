98610bfd26618ba6dab8b3a50b5e150764199542
Jorge Gil <jorgegilcavazos@gmail.com>
2016-04-25 23:21:17 -0500

    Fix back button

--- AppYoConstruyo/www/js/controllers.js
+++ AppYoConstruyo/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $http, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $http, $ionicModal, $timeout, $ionicHistory) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,17 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+
+  $scope.noBack = function() {
+    console.log('go back', ':)');
+    $ionicHistory.nextViewOptions({
+      disableAnimate: false,
+    });
+    $ionicHistory.goBack(-1);
+    //$ionicHistory.removeBackView();
+  };
+
 
   // Form data for the login modal
   $scope.data = {};
